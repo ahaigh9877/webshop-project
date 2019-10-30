82c839bb985264f1b49dee00b1ed1541fa1bcb00
mbosselut <duchmol7@hotmail.com>
2019-10-30 16:15:27 +0100

    Fixed CSS on Product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,28 +1,26 @@
-import React from "react";
+import React from 'react';
 
 const ProductCard = props => {
-
-  return (
-    <div>
-      <img src={props.img} alt={props.name} />
-      <p>
-        <strong>{props.name}</strong>
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nibh
-        venenatis, aliquam diam eget, tempus metus.{" "}
-      </p>
-<p>Price: {props.price / 100}€</p>    
-      <button
-        onClick={event =>
-          props.handleClick(event, props.name, props.img, props.price)
-        }
-      >
-        Add to cart
-      </button>
-    </div>
-  );
-
+    return (
+        <div className="productCard">
+            <img src={props.img} alt={props.name} />
+            <p>
+                <strong>{props.name}</strong>
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                vitae nibh venenatis, aliquam diam eget, tempus metus.{' '}
+            </p>
+            <p>Price: {props.price / 100}€</p>
+            <button
+                onClick={event =>
+                    props.handleClick(event, props.name, props.img, props.price)
+                }
+            >
+                Add to cart
+            </button>
+        </div>
+    );
 };
 
 export default ProductCard;

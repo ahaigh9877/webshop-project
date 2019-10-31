701677a5a7df79f1a71efd16f255b5ef85766b90
ahaigh9877 <alastairhaigh@me.com>
2019-10-31 09:08:04 +0100

    logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import logo from "../mms-logo-vertical.svg";
 
 class Header extends Component {
-    render() {
-        return (
-            <header>
-                <Link to="/">
-                    <img className="logoImg" alt="logo" src=""></img>
-                </Link>
+  render() {
+    return (
+      <header>
+        <Link to="/">
+          <img className="logoImg" alt="logo" src={logo}></img>
+        </Link>
 
-                <a className="topLink" href="#">
-                    Category 1
-                </a>
-                <a className="topLink" href="#">
-                    Category 2
-                </a>
-                <a className="topLink" href="#">
-                    Category 3
-                </a>
-                <Link to="/checkout" className="topLink">
-                    Shopping Cart
-                </Link>
-                {/* <a className="topLink" href="#">
+        <a className="topLink" href="#">
+          Category 1
+        </a>
+        <a className="topLink" href="#">
+          Category 2
+        </a>
+        <a className="topLink" href="#">
+          Category 3
+        </a>
+        <Link to="/checkout" className="topLink">
+          Shopping Cart
+        </Link>
+        {/* <a className="topLink" href="#">
           Shopping Cart
         </a> */}
-            </header>
-        );
-    }
+      </header>
+    );
+  }
 }
 
 export default connect()(Header);

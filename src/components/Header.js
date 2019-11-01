84b7829ecb45094622bd52e65bbdbe05e26b7cc8
ahaigh9877 <alastairhaigh@me.com>
2019-11-01 16:09:29 +0100

    fixed merge snafu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-
 import React, { Component, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
@@ -7,67 +6,88 @@ import shoppingcart from "../shopping-cart.svg";
 import shoppingcartactive from "../shopping-cart-active.svg";
 
 import {
-    filterBySearch,
-    resetFilters,
-    filterProducts
-} from '../actions/filterProducts';
-import { getProducts } from '../actions/getProducts';
-import { CSSTransition } from 'react-transition-group';
-import SearchForm from './SearchForm';
+  filterBySearch,
+  resetFilters,
+  filterProducts
+} from "../actions/filterProducts";
+import { getProducts } from "../actions/getProducts";
+import { CSSTransition } from "react-transition-group";
+import SearchForm from "./SearchForm";
 
 class Header extends Component {
-    state = {
-        searchTerm: null
-    };
-    handleSearch = event => {
-        this.setState({ searchTerm: event.target.value });
-    };
+  state = {
+    searchTerm: null
+  };
+  handleSearch = event => {
+    this.setState({ searchTerm: event.target.value });
+  };
 
-    handleSubmitSearch = event => {
-        event.preventDefault();
-        if (this.state.searchTerm) {
-            this.props.filterBySearch(
-                this.props.products,
-                this.state.searchTerm
-            );
-        }
-    };
-    categoryFilter = id => {
-        // console.log('filtering called', this.props.products, id);
-        return this.props.filterProducts(this.props.products, id);
-    };
+  handleSubmitSearch = event => {
+    event.preventDefault();
+    if (this.state.searchTerm) {
+      this.props.filterBySearch(this.props.products, this.state.searchTerm);
+    }
+  };
+  categoryFilter = id => {
+    // console.log('filtering called', this.props.products, id);
+    return this.props.filterProducts(this.props.products, id);
+  };
 
-    handleReset = () => {
-        this.props.getProducts();
-        this.props.resetFilters();
-    };
-    render() {
-        // console.log('header state: ', this.props.cartProducts);
-        return (
-            <header id="header">
-                <div id="logoLinkWrapper">
-                    <Link id="logoLink" to="/">
-                        <img
-                            onClick={() => this.handleReset()}
-                            id="logoImg"
-                            alt="logo"
-                            src={logo}
-                        ></img>
-                    </Link>
-                </div>
-                <div id="headerButtonWrapper">
-                    <div className="dropdown">
-                        <button className="dropButton">Categories</button>
-                        <div className="dropdownCategories">
-                            <Link to="/">
-                                <a
-                                    className="topLink"
-                                    onClick={() => this.categoryFilter(1)}
-                                >
-                                    Potions 'n' pills
-                                </a>
-                            </Link>
+  handleReset = () => {
+    this.props.getProducts();
+    this.props.resetFilters();
+  };
+  render() {
+    // console.log('header state: ', this.props.cartProducts);
+    return (
+      <header id="header">
+        <div id="logoLinkWrapper">
+          <Link id="logoLink" to="/">
+            <img
+              onClick={() => this.handleReset()}
+              id="logoImg"
+              alt="logo"
+              src={logo}
+            ></img>
+          </Link>
+        </div>
+        <div id="headerButtonWrapper">
+          <div className="dropdown">
+            <button className="dropButton">Categories</button>
+            <div className="dropdownCategories">
+              <Link to="/">
+                <a className="topLink" onClick={() => this.categoryFilter(1)}>
+                  Potions 'n' pills
+                </a>
+              </Link>
 
+              <Link to="/">
+                <a className="topLink" onClick={() => this.categoryFilter(2)}>
+                  Charms 'n' spells
+                </a>
+              </Link>
+              <Link to="/">
+                <a className="topLink" onClick={() => this.categoryFilter(3)}>
+                  Beatles merchandise
+                </a>
+              </Link>
+            </div>
+          </div>
+          {/* <div className="headerButton" id="searchWrapper"> */}
+          <div className="dropdown">
+            <button className="dropButton">Search</button>
+            <div className="dropdownCategories">
+              <SearchForm
+                className="headerButton"
+                // id="searchWrapper"
+                handleSearch={this.handleSearch}
+                handleSubmitSearch={this.handleSubmitSearch}
+                searchTerm={this.state.searchTerm}
+                handleReset={this.handleReset}
+              />
+            </div>
+          </div>
+        </div>
 
         <div id="cartLinkWrapper">
           <Link to="/checkout" id="cartLink">
@@ -90,9 +110,8 @@ class Header extends Component {
                       </p>
                     </div>
                   }
-
                 </div>
-
+              )}
 
               {this.props.cartProducts.length < 1 && (
                 <img
@@ -107,7 +126,6 @@ class Header extends Component {
       </header>
     );
   }
-
 }
 
 const mapStateToProps = state => {
@@ -118,8 +136,6 @@ const mapStateToProps = state => {
 };
 
 export default connect(
-
-    mapStateToProps,
-    { filterProducts, resetFilters, filterBySearch, getProducts }
-
+  mapStateToProps,
+  { filterProducts, resetFilters, filterBySearch, getProducts }
 )(Header);

73973bd8f82e23392fae572fa1fd729ca3949fc5
mbosselut <duchmol7@hotmail.com>
2019-11-01 15:16:13 +0100

    Sorting now works within categories

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistor, store } from './store';
 
+import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import "./App.css";
-import Header from "./components/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-import Footer from "./components/Footer";
-import ProductListGallery from "./components/ProductListGallery";
-import { Route } from "react-router-dom";
-import ShoppingCartContainer from "./components/ShoppingCartContainer";
+import Footer from './components/Footer';
+import ProductListGallery from './components/ProductListGallery';
+import { Route } from 'react-router-dom';
+import ShoppingCartContainer from './components/ShoppingCartContainer';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <div className="App">
-          <Header />
-          <main>
-            <Route exact path="/" component={ProductListGallery} />
-            <Route path="/checkout" component={ShoppingCartContainer} />
-          </main>
-          <Footer />
-        </div>
-      </PersistGate>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <div className="App">
+                    <Header />
+                    <main>
+                        <Route exact path="/" component={ProductListGallery} />
+                        <Route
+                            path="/checkout"
+                            component={ShoppingCartContainer}
+                        />
+                    </main>
+                    <Footer />
+                </div>
+            </PersistGate>
+        </Provider>
+    );
 }
 
 export default App;
--- src/components/ProductListGallery.js
+++ src/components/ProductListGallery.js
@@ -1,126 +1,154 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
-  setProducts,
-  getProducts,
-  sortLowToHigh,
-  sortHighToLow,
-  sortAToZ,
-  sortZToA
-} from "../actions/getProducts";
-import { addToCart } from "../actions/addToCart";
-import { filterBySearch, resetFilters } from "../actions/filterProducts";
-import ProductCard from "./ProductCard";
-import SearchForm from "./SearchForm";
-import DropdownButton from "react-bootstrap/DropdownButton";
-import Dropdown from "react-bootstrap/Dropdown";
-
-class ProductListGallery extends Component {
-  componentDidMount() {
-    this.props.getProducts();
-    window.addEventListener("scroll", this.resizeHeaderOnScroll);
-  }
-  resizeHeaderOnScroll() {
-    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 200,
-      headerEl = document.getElementById("header");
-    //headerBut = document.getElementsByClassName("headerButton");
-
-    if (distanceY > shrinkOn) {
-      headerEl.classList.add("smaller");
-      //headerBut.classList.add("smaller");
-    } else {
-      headerEl.classList.remove("smaller");
-      //headerBut.classList.add("smaller");
-    }
-  }
-
-  handleClick = (event, product, image, price, productId) => {
-    event.preventDefault();
-    this.props.addToCart(product, image, price, productId);
-  };
-
-  sortLowToHigh = () => this.props.sortLowToHigh(this.props.products);
-
-  sortHighToLow = () => this.props.sortHighToLow(this.props.products);
-
-  sortAToZ = () => this.props.sortAToZ(this.props.products);
-
-  sortZToA = () => this.props.sortZToA(this.props.products);
-
-  render() {
-    console.log("props.filteredProducts", this.props.filteredProducts);
-
-    const isFiltered = this.props.filteredProducts.length > 0;
-    const galleryDisplay = isFiltered
-      ? this.props.filteredProducts.map((product, index) => (
-          <ProductCard
-            key={index}
-            name={product.name}
-            description={product.description}
-            img={product.imageUrl}
-            price={product.price}
-            handleClick={this.handleClick}
-            productId={product.productId}
-          />
-        ))
-      : this.props.products.map((product, index) => (
-          <ProductCard
-            key={index}
-            name={product.name}
-            description={product.description}
-            img={product.imageUrl}
-            price={product.price}
-            handleClick={this.handleClick}
-            productId={product.productId}
-          />
-        ));
-
-    return (
-      <div>
-        <DropdownButton id="dropdown-item-button" title="Sort products">
-          <Dropdown.Item as="button" onClick={this.sortLowToHigh}>
-            Price : low to high
-          </Dropdown.Item>
-
-          <Dropdown.Item as="button" onClick={this.sortHighToLow}>
-            Price : high to low
-          </Dropdown.Item>
-
-          <Dropdown.Item as="button" onClick={this.sortAToZ}>
-            A - Z
-          </Dropdown.Item>
-
-          <Dropdown.Item as="button" onClick={this.sortZToA}>
-            Z - A
-          </Dropdown.Item>
-        </DropdownButton>
-        <div className="productListGallery">{galleryDisplay}</div>
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = state => {
-  return {
-    products: state.products,
-    filteredProducts: state.filteredProducts
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  {
     setProducts,
     getProducts,
-    addToCart,
-    filterBySearch,
-    resetFilters,
     sortLowToHigh,
     sortHighToLow,
     sortAToZ,
     sortZToA
-  }
+} from '../actions/getProducts';
+import { addToCart } from '../actions/addToCart';
+import { filterBySearch, resetFilters } from '../actions/filterProducts';
+import ProductCard from './ProductCard';
+import SearchForm from './SearchForm';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+class ProductListGallery extends Component {
+    componentDidMount() {
+        this.props.getProducts();
+        window.addEventListener('scroll', this.resizeHeaderOnScroll);
+    }
+    resizeHeaderOnScroll() {
+        const distanceY =
+                window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 200,
+            headerEl = document.getElementById('header');
+        //headerBut = document.getElementsByClassName("headerButton");
+
+        if (distanceY > shrinkOn) {
+            headerEl.classList.add('smaller');
+            //headerBut.classList.add("smaller");
+        } else {
+            headerEl.classList.remove('smaller');
+            //headerBut.classList.add("smaller");
+        }
+    }
+
+    handleClick = (event, product, image, price, productId) => {
+        event.preventDefault();
+        this.props.addToCart(product, image, price, productId);
+    };
+
+    sortLowToHigh = () => {
+        if (this.props.filteredProducts.length > 0) {
+            this.props.sortLowToHigh(this.props.filteredProducts);
+        } else {
+            this.props.sortLowToHigh(this.props.products);
+        }
+    };
+
+    sortHighToLow = () => {
+        console.log('products: ', this.props.filteredProducts);
+        console.log('length: ', this.props.filteredProducts.length);
+
+        if (this.props.filteredProducts.length > 0) {
+            this.props.sortHighToLow(this.props.filteredProducts);
+        } else {
+            this.props.sortHighToLow(this.props.products);
+        }
+    };
+
+    sortAToZ = () => {
+        if (this.props.filteredProducts.length > 0) {
+            this.props.sortAToZ(this.props.filteredProducts);
+        } else {
+            this.props.sortAToZ(this.props.products);
+        }
+    };
+
+    sortZToA = () => {
+        if (this.props.filteredProducts.length > 0) {
+            this.props.sortZToA(this.props.filteredProducts);
+        } else {
+            this.props.sortZToA(this.props.products);
+        }
+    };
+
+    render() {
+        console.log('props.filteredProducts', this.props.filteredProducts);
+
+        const isFiltered = this.props.filteredProducts.length > 0;
+        const galleryDisplay = isFiltered
+            ? this.props.filteredProducts.map((product, index) => (
+                  <ProductCard
+                      key={index}
+                      name={product.name}
+                      description={product.description}
+                      img={product.imageUrl}
+                      price={product.price}
+                      handleClick={this.handleClick}
+                      productId={product.productId}
+                  />
+              ))
+            : this.props.products.map((product, index) => (
+                  <ProductCard
+                      key={index}
+                      name={product.name}
+                      description={product.description}
+                      img={product.imageUrl}
+                      price={product.price}
+                      handleClick={this.handleClick}
+                      productId={product.productId}
+                  />
+              ));
+
+        return (
+            <div>
+                <DropdownButton id="dropdown-item-button" title="Sort products">
+                    <Dropdown.Item as="button" onClick={this.sortLowToHigh}>
+                        Price : low to high
+                    </Dropdown.Item>
+
+                    <Dropdown.Item as="button" onClick={this.sortHighToLow}>
+                        Price : high to low
+                    </Dropdown.Item>
+
+                    <Dropdown.Item as="button" onClick={this.sortAToZ}>
+                        A - Z
+                    </Dropdown.Item>
+
+                    <Dropdown.Item as="button" onClick={this.sortZToA}>
+                        Z - A
+                    </Dropdown.Item>
+                </DropdownButton>
+                <div className="productListGallery">{galleryDisplay}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        products: state.products,
+        filteredProducts: state.filteredProducts
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    {
+        setProducts,
+        getProducts,
+        addToCart,
+        filterBySearch,
+        resetFilters,
+        sortLowToHigh,
+        sortHighToLow,
+        sortAToZ,
+        sortZToA
+    }
 )(ProductListGallery);
 
 // export default ProductListGallery;

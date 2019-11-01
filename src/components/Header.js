56523bd68691c20a871563344c8c3776ab1edf38
mbosselut <duchmol7@hotmail.com>
2019-11-01 10:50:53 +0100

    Implementing sorting of products

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,94 +1,107 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { Link } from "react-router-dom";
-import logo from "../mms-logo-vertical.svg";
-import shoppingcart from "../shopping-cart.svg";
-import { resetFilters } from "../actions/filterProducts";
-import { filterProducts } from "../actions/filterProducts";
-import { CSSTransition } from "react-transition-group";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import logo from '../mms-logo-vertical.svg';
+import shoppingcart from '../shopping-cart.svg';
+import { resetFilters } from '../actions/filterProducts';
+import { filterProducts } from '../actions/filterProducts';
+import { CSSTransition } from 'react-transition-group';
 
 class Header extends Component {
-  categoryFilter = id => {
-    console.log("filtering called", this.props.products, id);
-    return this.props.filterProducts(this.props.products, id);
-  };
+    categoryFilter = id => {
+        // console.log("filtering called", this.props.products, id);
+        return this.props.filterProducts(this.props.products, id);
+    };
 
-  handleReset = () => {
-    this.props.resetFilters();
-  };
-  render() {
-    console.log("header state: ", this.props.cartProducts);
-    return (
-      <header id="header">
-        <div id="logoLinkWrapper">
-          <Link id="logoLink" to="/">
-            <img
-              onClick={() => this.handleReset()}
-              id="logoImg"
-              alt="logo"
-              src={logo}
-            ></img>
-          </Link>
-        </div>
-        <div id="headerButtonWrapper">
-          <div className="headerButton" className="dropdown">
-            <button className="dropButton">Categories</button>
-            <div className="dropdownCategories">
-              <Link to="/">
-                <a className="topLink" onClick={() => this.categoryFilter(1)}>
-                  Potions 'n' pills
-                </a>
-              </Link>
+    handleReset = () => {
+        this.props.resetFilters();
+    };
+    render() {
+        // console.log("header state: ", this.props.cartProducts);
+        return (
+            <header id="header">
+                <div id="logoLinkWrapper">
+                    <Link id="logoLink" to="/">
+                        <img
+                            onClick={() => this.handleReset()}
+                            id="logoImg"
+                            alt="logo"
+                            src={logo}
+                        ></img>
+                    </Link>
+                </div>
+                <div id="headerButtonWrapper">
+                    <div className="headerButton" className="dropdown">
+                        <button className="dropButton">Categories</button>
+                        <div className="dropdownCategories">
+                            <Link to="/">
+                                <a
+                                    className="topLink"
+                                    onClick={() => this.categoryFilter(1)}
+                                >
+                                    Potions 'n' pills
+                                </a>
+                            </Link>
 
-              <Link to="/">
-                <a className="topLink" onClick={() => this.categoryFilter(2)}>
-                  Charms 'n' spells
-                </a>
-              </Link>
-              <Link to="/">
-                <a className="topLink" onClick={() => this.categoryFilter(3)}>
-                  Beatles merchandise
-                </a>
-              </Link>
-            </div>
-          </div>
-          <div className="headerButton" id="searchWrapper">
-            Search
-          </div>
-        </div>
-        <CSSTransition>
-          <div id="cartLinkWrapper">
-            <Link to="/checkout" id="cartLink">
-              <div id="cartIconBox">
-                {console.log(this.props.cartProducts)}
-                {this.props.cartProducts.length > 0 && (
-                  <div id="quantityBadge">
-                    <p id="quantityNumber">
-                      {this.props.cartProducts.reduce(
-                        (a, b) => a + b.quantity,
-                        0
-                      )}
-                    </p>
-                  </div>
-                )}
-                <img id="shoppingCart" src={shoppingcart} alt="shopping cart" />
-              </div>
-            </Link>
-          </div>
-        </CSSTransition>
-      </header>
-    );
-  }
+                            <Link to="/">
+                                <a
+                                    className="topLink"
+                                    onClick={() => this.categoryFilter(2)}
+                                >
+                                    Charms 'n' spells
+                                </a>
+                            </Link>
+                            <Link to="/">
+                                <a
+                                    className="topLink"
+                                    onClick={() => this.categoryFilter(3)}
+                                >
+                                    Beatles merchandise
+                                </a>
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="headerButton" id="searchWrapper">
+                        Search
+                    </div>
+                </div>
+                <CSSTransition>
+                    <div id="cartLinkWrapper">
+                        <Link to="/checkout" id="cartLink">
+                            <div id="cartIconBox">
+                                {/* {console.log(this.props.cartProducts)} */}
+                                {this.props.cartProducts.length > 0 && (
+                                    <div id="quantityBadge">
+                                        <p id="quantityNumber">
+                                            {this.props.cartProducts.reduce(
+                                                (a, b) => a + b.quantity,
+                                                0
+                                            )}
+                                        </p>
+                                    </div>
+                                )}
+                                <img
+                                    id="shoppingCart"
+                                    src={shoppingcart}
+                                    alt="shopping cart"
+                                />
+                            </div>
+                        </Link>
+                    </div>
+                </CSSTransition>
+            </header>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return {
-    products: state.products,
-    cartProducts: state.shoppingCart
-  };
+    return {
+        products: state.products,
+        cartProducts: state.shoppingCart
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  { filterProducts, resetFilters }
+    mapStateToProps,
+    { filterProducts, resetFilters }
 )(Header);
--- src/components/ProductListGallery.js
+++ src/components/ProductListGallery.js
@@ -1,130 +1,178 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { setProducts, getProducts } from "../actions/getProducts";
-import { addToCart } from "../actions/addToCart";
-import { filterBySearch, resetFilters } from "../actions/filterProducts";
-import ProductCard from "./ProductCard";
-import SearchForm from "./SearchForm";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+    setProducts,
+    getProducts,
+    sortLowToHigh,
+    sortHighToLow,
+    sortAToZ,
+    sortZToA
+} from '../actions/getProducts';
+import { addToCart } from '../actions/addToCart';
+import { filterBySearch, resetFilters } from '../actions/filterProducts';
+import ProductCard from './ProductCard';
+import SearchForm from './SearchForm';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 class ProductListGallery extends Component {
-  state = {
-    searchTerm: null
-  };
+    state = {
+        searchTerm: null
+    };
 
-  componentDidMount() {
-    this.props.getProducts();
-    window.addEventListener("scroll", this.resizeHeaderOnScroll);
-  }
-  resizeHeaderOnScroll() {
-    const distanceY = window.pageYOffset || document.documentElement.scrollTop,
-      shrinkOn = 200,
-      headerEl = document.getElementById("header");
-    //headerBut = document.getElementsByClassName("headerButton");
-
-    if (distanceY > shrinkOn) {
-      headerEl.classList.add("smaller");
-      //headerBut.classList.add("smaller");
-    } else {
-      headerEl.classList.remove("smaller");
-      //headerBut.classList.add("smaller");
+    componentDidMount() {
+        this.props.getProducts();
+        window.addEventListener('scroll', this.resizeHeaderOnScroll);
     }
-  }
+    resizeHeaderOnScroll() {
+        const distanceY =
+                window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 200,
+            headerEl = document.getElementById('header');
+        //headerBut = document.getElementsByClassName("headerButton");
 
-  handleSearch = event => {
-    //using setState here for now. May use action later... or just on submit perhaps?
-
-    this.setState({ searchTerm: event.target.value });
-  };
-
-  handleSubmitSearch = event => {
-    event.preventDefault();
-    this.props.filterBySearch(this.props.products, this.state.searchTerm);
-    // this.setState({ foundProducts: foundProducts });
-  };
-
-  handleClick = (event, product, image, price, productId) => {
-    event.preventDefault();
-    this.props.addToCart(product, image, price, productId);
-  };
-
-  handleReset = event => {
-    console.log("RESET");
-    this.props.resetFilters();
-    this.setState({ searchTerm: null });
-    console.log(this.state);
-  };
-
-  render() {
-    console.log("props.filteredProducts", this.props.filteredProducts);
-
-    const isFiltered = this.props.filteredProducts.length > 0;
-    let galleryDisplay;
-
-    if (isFiltered) {
-      galleryDisplay = this.props.filteredProducts.map((product, index) => (
-        <ProductCard
-          key={index}
-          name={product.name}
-          description={product.description}
-          img={product.imageUrl}
-          price={product.price}
-          handleClick={this.handleClick}
-          productId={product.productId}
-        />
-      ));
-    } else {
-      galleryDisplay = this.props.products.map((product, index) => (
-        <ProductCard
-          key={index}
-          name={product.name}
-          description={product.description}
-          img={product.imageUrl}
-          price={product.price}
-          handleClick={this.handleClick}
-          productId={product.productId}
-        />
-      ));
+        if (distanceY > shrinkOn) {
+            headerEl.classList.add('smaller');
+            //headerBut.classList.add("smaller");
+        } else {
+            headerEl.classList.remove('smaller');
+            //headerBut.classList.add("smaller");
+        }
     }
 
-    return (
-      <div>
-        <SearchForm
-          handleSearch={this.handleSearch}
-          handleSubmitSearch={this.handleSubmitSearch}
-          searchTerm={this.state.searchTerm}
-          handleReset={this.handleReset}
-        />
-        <div className="productListGallery">{galleryDisplay}</div>
-        <div className="productListGallery">
-          {this.state.foundProducts &&
-            this.state.foundProducts.map((product, index) => (
-              <ProductCard
-                key={index}
-                na
-                me={product.name}
-                description={product.description}
-                img={product.imageUrl}
-                price={product.price}
-                handleClick={this.handleClick}
-                productId={product.productId}
-              />
-            ))}
-        </div>
-      </div>
-    );
-  }
+    handleSearch = event => {
+        //using setState here for now. May use action later... or just on submit perhaps?
+
+        this.setState({ searchTerm: event.target.value });
+    };
+
+    handleSubmitSearch = event => {
+        event.preventDefault();
+        this.props.filterBySearch(this.props.products, this.state.searchTerm);
+        // this.setState({ foundProducts: foundProducts });
+    };
+
+    handleClick = (event, product, image, price, productId) => {
+        event.preventDefault();
+        this.props.addToCart(product, image, price, productId);
+    };
+
+    handleReset = event => {
+        console.log('RESET');
+        this.props.resetFilters();
+        this.setState({ searchTerm: null });
+        console.log(this.state);
+    };
+
+    sortLowToHigh = () => this.props.sortLowToHigh(this.props.products);
+
+    sortHighToLow = () => this.props.sortHighToLow(this.props.products);
+
+    sortAToZ = () => this.props.sortAToZ(this.props.products);
+
+    sortZToA = () => this.props.sortZToA(this.props.products);
+
+    render() {
+        const isFiltered = this.props.filteredProducts.length > 0;
+        let galleryDisplay;
+
+        if (isFiltered) {
+            galleryDisplay = this.props.filteredProducts.map(
+                (product, index) => (
+                    <ProductCard
+                        key={index}
+                        name={product.name}
+                        description={product.description}
+                        img={product.imageUrl}
+                        price={product.price}
+                        handleClick={this.handleClick}
+                        productId={product.productId}
+                    />
+                )
+            );
+        } else {
+            galleryDisplay = this.props.products.map((product, index) => (
+                <ProductCard
+                    key={index}
+                    name={product.name}
+                    description={product.description}
+                    img={product.imageUrl}
+                    price={product.price}
+                    handleClick={this.handleClick}
+                    productId={product.productId}
+                />
+            ));
+        }
+
+        return (
+            <div>
+                <SearchForm
+                    handleSearch={this.handleSearch}
+                    handleSubmitSearch={this.handleSubmitSearch}
+                    searchTerm={this.state.searchTerm}
+                    handleReset={this.handleReset}
+                />
+                <DropdownButton id="dropdown-item-button" title="Sort products">
+                    <Dropdown.Item
+                        as="button"
+                        onClick={() => this.sortLowToHigh()}
+                    >
+                        Price : low to high
+                    </Dropdown.Item>
+                    <Dropdown.Item
+                        as="button"
+                        onClick={() => this.sortHighToLow()}
+                    >
+                        Price : high to low
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button" onClick={() => this.sortAToZ()}>
+                        A - Z
+                    </Dropdown.Item>
+                    <Dropdown.Item as="button" onClick={() => this.sortZToA()}>
+                        Z - A
+                    </Dropdown.Item>
+                </DropdownButton>
+                <div className="productListGallery">{galleryDisplay}</div>
+                <div className="productListGallery">
+                    {this.state.foundProducts &&
+                        this.state.foundProducts.map((product, index) => (
+                            <ProductCard
+                                key={index}
+                                na
+                                me={product.name}
+                                description={product.description}
+                                img={product.imageUrl}
+                                price={product.price}
+                                handleClick={this.handleClick}
+                                productId={product.productId}
+                            />
+                        ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = state => {
-  return {
-    products: state.products,
-    filteredProducts: state.filteredProducts
-  };
+    return {
+        products: state.products,
+        filteredProducts: state.filteredProducts
+    };
 };
 
 export default connect(
-  mapStateToProps,
-  { setProducts, getProducts, addToCart, filterBySearch, resetFilters }
+    mapStateToProps,
+    {
+        setProducts,
+        getProducts,
+        addToCart,
+        filterBySearch,
+        resetFilters,
+        sortLowToHigh,
+        sortHighToLow,
+        sortAToZ,
+        sortZToA
+    }
 )(ProductListGallery);
 
 // export default ProductListGallery;
